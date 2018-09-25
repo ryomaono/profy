2f7e95024aa8e34c70d2e5222f604971bcd109b4
小野稜馬 <romayfilm5@gmail.com>
2018-09-24 22:34:38 -0700

    relation   models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,15 @@
 class Answer < ActiveRecord::Base
+  #callback
+  after_create :create_feed_content
+  #validaton
+  validates_presence_of :user_id, :text
+
   belongs_to :question
   belongs_to :user
   has_one :feed_content, as: :content, dependent: :destroy
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: question.group_id, updated_at: updated_at)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  #callback
+  after_create :create_feed_content
   #association
   belongs_to :user
   belongs_to :group
@@ -9,5 +11,10 @@ class Question < ActiveRecord::Base
 
   def user_answer(user_id)
     Answer.find_by(user_id: user_id, question_id: id)
+  end
+
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: group_id, updated_at: updated_at)
   end
 end
